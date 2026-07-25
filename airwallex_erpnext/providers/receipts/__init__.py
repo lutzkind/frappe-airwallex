9aa8993357bd7d454e8c25b8536e810b608a3753
lutzkind <132444134+lutzkind@users.noreply.github.com>
2026-07-25 14:07:24 +0700

    feat: register Frappe email receipt provider

--- airwallex_erpnext/providers/receipts/__init__.py
+++ airwallex_erpnext/providers/receipts/__init__.py
@@ -1,5 +1,6 @@
 from airwallex_erpnext.providers.receipts.airwallex import AirwallexReceiptProvider
 from airwallex_erpnext.providers.receipts.compatibility import CompatibilityReceiptProvider
+from airwallex_erpnext.providers.receipts.frappe_email import FrappeEmailReceiptProvider
 from airwallex_erpnext.providers.receipts.imap import IMAPReceiptProvider
 from airwallex_erpnext.providers.receipts.manual import ManualReceiptProvider
 
